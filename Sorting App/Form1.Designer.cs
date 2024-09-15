8e7ee5270470ad37f440120368d8aebab4fa98e9
Bitahua <87811454+Bita404@users.noreply.github.com>
2024-09-15 17:30:03 +0330

    Add files via upload

---
+++ Sorting App/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Sorting_App
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxSortType = new System.Windows.Forms.GroupBox();
+            this.radioButtonDescending = new System.Windows.Forms.RadioButton();
+            this.radioButtonAscending = new System.Windows.Forms.RadioButton();
+            this.labelUnsortedData = new System.Windows.Forms.Label();
+            this.labelSortedData = new System.Windows.Forms.Label();
+            this.labelName1 = new System.Windows.Forms.Label();
+            this.labelName2 = new System.Windows.Forms.Label();
+            this.saveFileDialogmain = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.groupBoxSortType.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox1.Location = new System.Drawing.Point(22, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 396);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(382, 67);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 396);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.MediumPurple;
+            this.menuStrip1.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(529, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentInfoToolStripMenuItem,
+            this.toolStripMenuItem2});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.infoToolStripMenuItem.Text = "&Info";
+            // 
+            // studentInfoToolStripMenuItem
+            // 
+            this.studentInfoToolStripMenuItem.Name = "studentInfoToolStripMenuItem";
+            this.studentInfoToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.studentInfoToolStripMenuItem.Text = "Student info";
+            this.studentInfoToolStripMenuItem.Click += new System.EventHandler(this.studentInfoToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(155, 22);
+            this.toolStripMenuItem2.Text = "?";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // groupBoxSortType
+            // 
+            this.groupBoxSortType.BackColor = System.Drawing.Color.Purple;
+            this.groupBoxSortType.Controls.Add(this.radioButtonDescending);
+            this.groupBoxSortType.Controls.Add(this.radioButtonAscending);
+            this.groupBoxSortType.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxSortType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBoxSortType.Location = new System.Drawing.Point(175, 149);
+            this.groupBoxSortType.Name = "groupBoxSortType";
+            this.groupBoxSortType.Size = new System.Drawing.Size(168, 88);
+            this.groupBoxSortType.TabIndex = 3;
+            this.groupBoxSortType.TabStop = false;
+            this.groupBoxSortType.Text = "Sort type";
+            // 
+            // radioButtonDescending
+            // 
+            this.radioButtonDescending.AutoSize = true;
+            this.radioButtonDescending.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.radioButtonDescending.ForeColor = System.Drawing.Color.Indigo;
+            this.radioButtonDescending.Location = new System.Drawing.Point(6, 58);
+            this.radioButtonDescending.Name = "radioButtonDescending";
+            this.radioButtonDescending.Size = new System.Drawing.Size(130, 24);
+            this.radioButtonDescending.TabIndex = 1;
+            this.radioButtonDescending.TabStop = true;
+            this.radioButtonDescending.Text = "Descending";
+            this.radioButtonDescending.UseVisualStyleBackColor = false;
+            this.radioButtonDescending.CheckedChanged += new System.EventHandler(this.radioButtonDescending_CheckedChanged);
+            // 
+            // radioButtonAscending
+            // 
+            this.radioButtonAscending.AutoSize = true;
+            this.radioButtonAscending.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.radioButtonAscending.ForeColor = System.Drawing.Color.Indigo;
+            this.radioButtonAscending.Location = new System.Drawing.Point(6, 29);
+            this.radioButtonAscending.Name = "radioButtonAscending";
+            this.radioButtonAscending.Size = new System.Drawing.Size(121, 24);
+            this.radioButtonAscending.TabIndex = 0;
+            this.radioButtonAscending.TabStop = true;
+            this.radioButtonAscending.Text = "Ascending";
+            this.radioButtonAscending.UseVisualStyleBackColor = false;
+            this.radioButtonAscending.CheckedChanged += new System.EventHandler(this.radioButtonAscending_CheckedChanged);
+            // 
+            // labelUnsortedData
+            // 
+            this.labelUnsortedData.AutoSize = true;
+            this.labelUnsortedData.BackColor = System.Drawing.Color.Purple;
+            this.labelUnsortedData.Font = new System.Drawing.Font("Lucida Calligraphy", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUnsortedData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.labelUnsortedData.Location = new System.Drawing.Point(19, 42);
+            this.labelUnsortedData.Name = "labelUnsortedData";
+            this.labelUnsortedData.Size = new System.Drawing.Size(127, 17);
+            this.labelUnsortedData.TabIndex = 4;
+            this.labelUnsortedData.Text = "Unsorted Data";
+            // 
+            // labelSortedData
+            // 
+            this.labelSortedData.AutoSize = true;
+            this.labelSortedData.Font = new System.Drawing.Font("Lucida Calligraphy", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSortedData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.labelSortedData.Location = new System.Drawing.Point(388, 42);
+            this.labelSortedData.Name = "labelSortedData";
+            this.labelSortedData.Size = new System.Drawing.Size(105, 17);
+            this.labelSortedData.TabIndex = 5;
+            this.labelSortedData.Text = "Sorted Data";
+            this.labelSortedData.Click += new System.EventHandler(this.labelSortedData_Click);
+            // 
+            // labelName1
+            // 
+            this.labelName1.AutoSize = true;
+            this.labelName1.BackColor = System.Drawing.Color.Violet;
+            this.labelName1.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName1.ForeColor = System.Drawing.Color.SlateBlue;
+            this.labelName1.Location = new System.Drawing.Point(157, 42);
+            this.labelName1.Name = "labelName1";
+            this.labelName1.Size = new System.Drawing.Size(209, 50);
+            this.labelName1.TabIndex = 6;
+            this.labelName1.Text = "Sorting";
+            this.labelName1.Click += new System.EventHandler(this.labelName1_Click);
+            // 
+            // labelName2
+            // 
+            this.labelName2.AutoSize = true;
+            this.labelName2.BackColor = System.Drawing.Color.Violet;
+            this.labelName2.Font = new System.Drawing.Font("Ravie", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName2.ForeColor = System.Drawing.Color.SlateBlue;
+            this.labelName2.Location = new System.Drawing.Point(204, 82);
+            this.labelName2.Name = "labelName2";
+            this.labelName2.Size = new System.Drawing.Size(115, 50);
+            this.labelName2.TabIndex = 7;
+            this.labelName2.Text = "App";
+            this.labelName2.Click += new System.EventHandler(this.labelName2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(529, 481);
+            this.Controls.Add(this.labelName2);
+            this.Controls.Add(this.labelName1);
+            this.Controls.Add(this.labelSortedData);
+            this.Controls.Add(this.labelUnsortedData);
+            this.Controls.Add(this.groupBoxSortType);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sorting app (by s9919314)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBoxSortType.ResumeLayout(false);
+            this.groupBoxSortType.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBoxSortType;
+        private System.Windows.Forms.RadioButton radioButtonDescending;
+        private System.Windows.Forms.RadioButton radioButtonAscending;
+        private System.Windows.Forms.Label labelUnsortedData;
+        private System.Windows.Forms.Label labelSortedData;
+        private System.Windows.Forms.Label labelName1;
+        private System.Windows.Forms.Label labelName2;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentInfoToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogmain;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ Sorting App/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting_App
+{
+    public partial class FormMain : Form
+    {
+        string fileName = null;
+        Boolean isChanged;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void labelName1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonDescending_CheckedChanged(object sender, EventArgs e)
+        {
+            long[] linesAsNumbers = new long[textBox1.Lines.Length];
+
+            long i = 0;
+            foreach (string line in textBox1.Lines)
+            {
+                linesAsNumbers[i++] = Convert.ToInt64(line);
+            }
+            Array.Sort(linesAsNumbers);
+            Array.Reverse(linesAsNumbers);
+
+            textBox2.Text = String.Join<long>("-", linesAsNumbers);
+        }
+
+        private void labelName2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelSortedData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void studentInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("- Done by Bita Saeed s9919314 -" + "\n" + " Thanks for choosing this app :D"+"\n"+"             Hope you enjoy!");
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileName == null)
+            {
+                saveFileDialogmain.DefaultExt = "txt";
+                saveFileDialogmain.ShowDialog();
+                if (saveFileDialogmain.ShowDialog() == DialogResult.Cancel)
+                    return;
+
+                fileName = saveFileDialogmain.FileName;
+                this.Text = fileName;
+            }
+            System.IO.File.WriteAllText(fileName, textBox2.Text);
+            isChanged = false;
+        }
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isChanged == true)
+            {
+                DialogResult dr;
+                dr = MessageBox.Show("Do you want to Save this file ?", "save...", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                    saveToolStripMenuItem_Click(null, null);
+            }
+            textBox2.Text = "";
+            this.Text = "sorting app num";
+            fileName = null;
+            isChanged = false;
+        }
+
+        
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)&& (e.KeyChar != '-'))
+    
+
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('-') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            isChanged = true;
+        }
+
+        private void radioButtonAscending_CheckedChanged(object sender, EventArgs e)
+        {
+            long[] linesAsNumbers = new long[textBox1.Lines.Length];
+
+            long i = 0;
+            foreach (string line in textBox1.Lines)
+            {
+                linesAsNumbers[i++] = Convert.ToInt64(line);
+            }
+            Array.Sort(linesAsNumbers);
+
+            textBox2.Text = String.Join<long>( "-" ,linesAsNumbers);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newToolStripMenuItem_Click(null, null);
+            openFileDialog1.Filter = "text File|*.txt|C# source File|*.cs|All Files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            fileName = openFileDialog1.FileName;
+            textBox2.Text = System.IO.File.ReadAllText(fileName);
+            isChanged = false;
+            this.Text = fileName;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.IO.File.WriteAllText("layout.txt", textBox2.Name);
+            newToolStripMenuItem_Click(null, null);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileName = null;
+            saveToolStripMenuItem_Click(null, null);
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("even though I used \\n for my array result" + "\n" + "It didnt changes anything at all :(");
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
